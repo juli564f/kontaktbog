27acabb745a40f3cfcc95fbdf9c381afcdacaef3
AstaCarl <cph-ac300@cphbusiness.dk>
2023-10-03 11:00:31 +0200

    validering

--- src/components/Opretkontakt.jsx
+++ src/components/Opretkontakt.jsx
@@ -6,7 +6,7 @@ import { Contact } from '../entities/contact';
 import "./opretkontakt.css"
 
 //Modtager props
-export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts }) {
+export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts}) {
 
 //Deklarerer state variabler med en tom "string", setFirstname osv. er en funktion som kan bruges til at ændre state
   const [firstname, setFirstname] = useState('');
@@ -15,6 +15,7 @@ export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts 
   const [phone, setPhone] = useState('');
   const [company, setCompany] = useState('');
   const [position, setPosition] = useState('');
+  const [touchedContact, setTouchedContact] = useState(false);
 
 //Funktioner som holder øje med ændringerer i value attribut i input felt
   const handleFirstnameChange = (e) => {
@@ -39,22 +40,36 @@ export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts 
   //funktion til at tilføje ny kontakt
   const handleAddContactClick = () =>{
     //Renderer et nyt objekt til array
-  const newContact = new Contact(firstname, lastname, email, phone, company, position);
-    //Opdaterer array med ny kontakt som indeholder firstnamt, lastname
-    setContacts([...contacts, newContact]);
-    //Tømmer input felterne efter click
-    setFirstname(''); 
-    setLastname('');
-    setEmail('');
-    setPhone('');
-    setCompany('');
-    setPosition('');
-    handleCloseModal()
+    if(isValid){
+      const newContact = new Contact(firstname, lastname, email, phone, company, position);
+        //   //Opdaterer array med ny kontakt som indeholder firstnamt, lastname
+      setContacts([...contacts, newContact]);
+      //   //Tømmer input felterne efter click
+      setFirstname(''); 
+      setLastname('');
+      setEmail('');
+      setPhone('');
+      setCompany('');
+      setPosition('');
+      handleCloseModal();
+    } else {
+      setTouchedContact(true);
+    }
 };
+//Funktion til at checke om input felterne er fyldt ud
+  let isValid = firstname.trim() !== '' &&
+  lastname.trim() !== '' &&
+  email.trim() !== '' &&
+  phone.trim() !== '' &&
+  company.trim() !== '' &&
+  position.trim() !== '';
+
+
 //Funktion som håndterer lukning af modal på click.
   function handleCloseModal() {
         setIsOpen(false);
-  };
+  }
+  
 
 //Modal style
   const customStyles = {
@@ -66,7 +81,7 @@ export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts 
       marginRight: '-50%',
       width: '80%',
       height: '80%',
-      },
+      }
   };
 
   return (
@@ -76,15 +91,27 @@ export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts 
             <div className="closeBtn">
               <MyBtn buttonText={"Close"} onClick={handleCloseModal}/>
             </div>
-                <input type="text" value={firstname} onChange={handleFirstnameChange} placeholder='Firstname' />
-                <input type="text" value={lastname} onChange={handleLastnameChange} placeholder='Lastname' />
-                <input type="text" value={email} onChange={handleEmailChange} placeholder='Email-address'/>
-                <input type="text" value={phone} onChange={handlePhoneChange} placeholder='Phone'/>
-                <input type="text" value={company} onChange={handleCompanyChange} placeholder='Company' />
-                <input type="text" value={position} onChange={handlePositionChange} placeholder='Position' />
+                <input type="text" value={firstname} onChange={handleFirstnameChange} placeholder='Firstname' 
+                onBlur={() => setTouchedContact(true)}/>
+                <input type="text" value={lastname} onChange={handleLastnameChange} placeholder='Lastname' 
+                onBlur={() => setTouchedContact(true)}/>
+                <input type="text" value={email} onChange={handleEmailChange} placeholder='Email-address'
+                onBlur={() => setTouchedContact(true)}/>
+                <input type="text" value={phone} onChange={handlePhoneChange} placeholder='Phone'
+                onBlur={() => setTouchedContact(true)}/>
+                <input type="text" value={company} onChange={handleCompanyChange} placeholder='Company' 
+                onBlur={() => setTouchedContact(true)}/>
+                <input type="text" value={position} onChange={handlePositionChange} placeholder='Position'           
+                onBlur={() => setTouchedContact(true)}/>
+                { !isValid && touchedContact &&
+          <div>
+            {/* Besked som viser at felterne skal udfyldes */}
+            Content cannot be empty. Please fill out the input.
+          </div>
+        }
               <MyBtn buttonText={"Create contact"} onClick={handleAddContactClick}/>
           </div>
         </ReactModal>
     </div>
-  );
+  )
 }
