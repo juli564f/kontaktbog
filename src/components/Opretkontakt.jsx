e1f8ce15a90c68fd79ebb875b02e8d9251dccd2f
AstaCarl <cph-ac300@cphbusiness.dk>
2023-10-04 14:03:38 +0200

    hej

--- src/components/Opretkontakt.jsx
+++ src/components/Opretkontakt.jsx
@@ -7,62 +7,73 @@ import "./opretkontakt.css"
 
 //Modtager props
 export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts}) {
+  const [ formValues, setFormValues ] = useState(new Contact('','','','','',''));
 
-//Deklarerer state variabler med en tom "string", setFirstname osv. er en funktion som kan bruges til at ændre state
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [company, setCompany] = useState('');
-  const [position, setPosition] = useState('');
+  function handleUpdateFormValues(event) {
+    console.log(formValues);
+    const name = event.target.name;
+    const value = event.target.value;
+    const result = { ...formValues, [name]: value };
+    setFormValues(result);
+  }
+
+// //Deklarerer state variabler med en tom "string", setFirstname osv. er en funktion som kan bruges til at ændre state
+//   const [firstname, setFirstname] = useState('');
+//   const [lastname, setLastname] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [phone, setPhone] = useState('');
+//   const [company, setCompany] = useState('');
+//   const [position, setPosition] = useState('');
   const [touchedContact, setTouchedContact] = useState(false);
 
 //Funktioner som holder øje med ændringerer i value attribut i input felt
-  const handleFirstnameChange = (e) => {
-    setFirstname(e.target.value);
-  };
-  const handleLastnameChange = (e) => {
-    setLastname(e.target.value);
-  };
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePhoneChange = (e) => {
-    setPhone(e.target.value);
-  };
-  const handleCompanyChange = (e) => {
-    setCompany(e.target.value);
-  };
-  const handlePositionChange = (e) => {
-    setPosition(e.target.value);
-  };
+  // const handleFirstnameChange = (e) => {
+  //   setFirstname(e.target.value);
+  // };
+  // const handleLastnameChange = (e) => {
+  //   setLastname(e.target.value);
+  // };
+  // const handleEmailChange = (e) => {
+  //   setEmail(e.target.value);
+  // };
+  // const handlePhoneChange = (e) => {
+  //   setPhone(e.target.value);
+  // };
+  // const handleCompanyChange = (e) => {
+  //   setCompany(e.target.value);
+  // };
+  // const handlePositionChange = (e) => {
+  //   setPosition(e.target.value);
+  // };
+
 
   //funktion til at tilføje ny kontakt
   const handleAddContactClick = () =>{
     //Renderer et nyt objekt til array
     if(isValid){
-      const newContact = new Contact(firstname, lastname, email, phone, company, position);
+      // const newContact = new Contact(firstname, lastname, email, phone, company, position);
+      const newContact = new Contact(
+        formValues.firstname,
+        formValues.lastname,
+        formValues.email,
+        formValues.phone,
+        formValues.company,
+        formValues.position,
+      );
         //   //Opdaterer array med ny kontakt som indeholder firstnamt, lastname
       setContacts([...contacts, newContact]);
       //   //Tømmer input felterne efter click
-      setFirstname(''); 
-      setLastname('');
-      setEmail('');
-      setPhone('');
-      setCompany('');
-      setPosition('');
+        setFormValues(new Contact('','','','','',''))
       handleCloseModal();
     } else {
       setTouchedContact(true);
     }
 };
-//Funktion til at checke om input felterne er fyldt ud
-  let isValid = firstname.trim() !== '' &&
-  lastname.trim() !== '' &&
-  email.trim() !== '' &&
-  phone.trim() !== '' &&
-  company.trim() !== '' &&
-  position.trim() !== '';
+
+      //Funktion til at checke om input felterne er fyldt ud
+      let isValid = formValues.firstname.trim() !== '';
+
+  
 
 
 //Funktion som håndterer lukning af modal på click.
@@ -91,18 +102,19 @@ export default function Opretkontakt({ isOpen, setIsOpen, setContacts, contacts}
             <div className="closeBtn">
               <MyBtn buttonText={"Close"} onClick={handleCloseModal}/>
             </div>
-                <input type="text" value={firstname} onChange={handleFirstnameChange} placeholder='Firstname' 
+                <input type="text" name='firstname' value={formValues.firstname} onChange={handleUpdateFormValues} placeholder='Firstname' 
                 onBlur={() => setTouchedContact(true)}/>
-                <input type="text" value={lastname} onChange={handleLastnameChange} placeholder='Lastname' 
+                <input type="text" name='lastname' value={formValues.lastname} onChange={handleUpdateFormValues} placeholder='Lastname' 
                 onBlur={() => setTouchedContact(true)}/>
-                <input type="text" value={email} onChange={handleEmailChange} placeholder='Email-address'
+                <input type="text" name='email' value={formValues.email} onChange={handleUpdateFormValues} placeholder='Email-address'
                 onBlur={() => setTouchedContact(true)}/>
-                <input type="text" value={phone} onChange={handlePhoneChange} placeholder='Phone'
+                <input type="text" name='phone' value={formValues.phone} onChange={handleUpdateFormValues} placeholder='Phone'
                 onBlur={() => setTouchedContact(true)}/>
-                <input type="text" value={company} onChange={handleCompanyChange} placeholder='Company' 
+                <input type="text" name='company' value={formValues.company} onChange={handleUpdateFormValues} placeholder='Company' 
                 onBlur={() => setTouchedContact(true)}/>
-                <input type="text" value={position} onChange={handlePositionChange} placeholder='Position'           
+                <input type="text" name='position' value={formValues.position} onChange={handleUpdateFormValues} placeholder='Position'           
                 onBlur={() => setTouchedContact(true)}/>
+
                 { !isValid && touchedContact &&
           <div>
             {/* Besked som viser at felterne skal udfyldes */}
